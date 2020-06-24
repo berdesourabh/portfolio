a4a348815d2461de5bd4446edc2291aef3f0929a
Sourabh Berde <sourabh.berde44@gmail.com>
2020-06-24 12:14:04 +0530

    commited build

---
+++ build/precache-manifest.e28b393ec5b76f24823e045403f2cca6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41dc82db1d62bbcc5271cc827b9abd9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9adc150488fe1a8eb9ef",
+    "url": "/portfolio/static/css/main.21ee9a9e.chunk.css"
+  },
+  {
+    "revision": "307073425f9a43777437",
+    "url": "/portfolio/static/js/2.5ab4bb21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.5ab4bb21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9adc150488fe1a8eb9ef",
+    "url": "/portfolio/static/js/main.e36a65d1.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "b77a278f3896de0236d5686862e69a9f",
+    "url": "/portfolio/static/media/docker.b77a278f.svg"
+  },
+  {
+    "revision": "af75c381d7edff7dfe198c66a3c7cd24",
+    "url": "/portfolio/static/media/github.af75c381.svg"
+  },
+  {
+    "revision": "aea8a07c42538085ae7919ee984a02ce",
+    "url": "/portfolio/static/media/img2.aea8a07c.jpg"
+  },
+  {
+    "revision": "e6a0ec06885c68b523d5c50ff4603dba",
+    "url": "/portfolio/static/media/kubernetes.e6a0ec06.svg"
+  },
+  {
+    "revision": "96889989ac4b01f4ecba1ab719890d4b",
+    "url": "/portfolio/static/media/postgres.96889989.svg"
+  },
+  {
+    "revision": "ddd11c2b829e68ee21b9030bf3cc2727",
+    "url": "/portfolio/static/media/reactjs.ddd11c2b.svg"
+  },
+  {
+    "revision": "305d8649c3f1c70e679fa74d1245e09f",
+    "url": "/portfolio/static/media/spring.305d8649.svg"
+  }
+]);
